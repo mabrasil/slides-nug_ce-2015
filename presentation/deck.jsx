e896b41eee801aaa7015d3c298f3ad397f2a087e
Matheus Brasil <matheus.brasil10@gmail.com>
2015-11-14 04:20:37 -0200

    Add more slides

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -10,9 +10,9 @@ import preloader from "../src/utils/preloader";
 import Interactive from "./interactive";
 
 const images = {
-  city: require("./city.jpg"),
-  kat: require("./kat.png"),
-  logo: require("./formidable-logo.svg")
+  automation: require("./img/gifs/automation.gif"),
+  kat: require("./img/gifs/kat.png"),
+  logo: require("./img/gifs/formidable-logo.svg")
 };
 
 preloader([images.city, images.kat]);
@@ -63,6 +63,57 @@ export default class extends React.Component {
             <ListItem><Appear fid="2">Sommelier de linguagens de programação</Appear></ListItem>
             <ListItem><Appear fid="3">Alquimista da vida</Appear></ListItem>
             <ListItem><Appear fid="4">Estudante de Informática no IFCE </Appear></ListItem>
+          </List>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+
+          <Text textSize="5em" margin="20px 0px 0px" bold caps textColor="tertiary">'Cês podem me achar:</Text>
+
+          <List>
+            <ListItem><Appear fid="1">twitter/@mabrasil_io</Appear></ListItem>
+            <ListItem><Appear fid="2">github/mabrasil</Appear></ListItem>
+            <ListItem><Appear fid="3">mabrasil.rocks</Appear></ListItem>
+          </List>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+
+          <Text textSize="5em" margin="20px 0px 0px" bold caps textColor="tertiary">Vamos falar de:</Text>
+
+          <List>
+            <ListItem><Appear fid="1">Automação?</Appear></ListItem>
+            <ListItem><Appear fid="2">O que usamos (e amamos)</Appear></ListItem>
+            <ListItem><Appear fid="3">"Problemas" com coisas que amamos</Appear></ListItem>
+            <ListItem><Appear fid="4">E se...</Appear></ListItem>
+            <ListItem><Appear fid="5">Galero usa?</Appear></ListItem>
+          </List>
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.automation.replace("/", "")} bgDarken={0.75}>
+          <Appear fid="2">
+            <Heading size={1} caps fit textColor="tertiary">
+              Automação
+            </Heading>
+          </Appear>
+          <Appear fid="3">
+            <Heading size={5} caps fit textColor="primary">
+              ???
+            </Heading>
+          </Appear>
+        </Slide>
+
+        <Slide transition={["fade"]} bgImage={images.automation.replace("/", "")} bgDarken={0.75}>
+
+          <Text textSize="5em" margin="20px 0px 0px" bold caps textColor="tertiary">Quem aqui precisa fazer?</Text>
+
+          <List>
+            <ListItem><Appear fid="1">Pré-processamento, concatenação, minificação e compressão de JS?</Appear></ListItem>
+            <ListItem><Appear fid="2">Pré-processamento/compilação de CSS</Appear></ListItem>
+            <ListItem><Appear fid="3">Geração de HTML (Template Engines/Static Gens)</Appear></ListItem>
+            <ListItem><Appear fid="4">Gerenciamento/Otimização de Imagens</Appear></ListItem>
+            <ListItem><Appear fid="5">Testes/Reports</Appear></ListItem>
+            <ListItem><Appear fid="6">...</Appear></ListItem>
           </List>
         </Slide>
 

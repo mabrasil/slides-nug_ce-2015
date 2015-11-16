25095568da520dba2c8d8bbf135d689be5c3c938
Matheus Brasil <matheus.brasil10@gmail.com>
2015-11-16 02:11:29 -0200

    Add some problems with current task runners

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -13,6 +13,13 @@ const images = {
   automation: require("./img/gifs/automation.gif")
   ,mind_blowing: require("./img/gifs/mind-blowing.gif")
   ,put_your_hands_up: require("./img/gifs/put-your-hands-up.gif")
+  ,gulp: require("./img/logos/task-runners/gulp.png")
+  ,grunt: require("./img/logos/task-runners/grunt.png")
+  ,broccoli: require("./img/logos/task-runners/broccoli.png")
+  ,but_what: require("./img/gifs/but-what.gif")
+  ,plugins: require("./img/other/plugins-hell.png")
+  ,surprises: require("./img/other/surprises.png")
+  ,substack: require("./img/other/substack.jpg")
 };
 
 preloader([images.city, images.kat]);
@@ -21,6 +28,12 @@ export default class extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={800} progress="bar" >
+
+      <Slide transition={["slide"]} bgColor="primary">
+        <Heading size={1} textColor="secondary">
+          !
+        </Heading>
+      </Slide>
 
       <Slide transition={["zoom"]} bgColor="primary">
 
@@ -91,11 +104,8 @@ export default class extends React.Component {
         </Slide>
 
         <Slide transition={["slide"]} bgImage={images.automation.replace("/", "")} bgDarken={0.75}>
-          <Heading size={2} caps fit textColor="tertiary">
-            Automação
-          </Heading>
-          <Heading size={5} caps fit textColor="primary">
-            ???
+          <Heading size={2} fit caps lineHeight={1} textColor="primary">
+            Automação???
           </Heading>
         </Slide>
 
@@ -111,6 +121,56 @@ export default class extends React.Component {
             <ListItem><Appear fid="5">Testes/Reports</Appear></ListItem>
             <ListItem><Appear fid="6">...</Appear></ListItem>
           </List>
+        </Slide>
+
+
+        <Slide transition={["slide"]} bgImage={images.automation.replace("/", "")} bgDarken={0.75}>
+          <Heading size={2} fit caps lineHeight={1} textColor="primary">
+            O que usamos???
+          </Heading>
+        </Slide>
+
+        <Slide transition={["zoom", "fade"]} bgColor="white">
+
+          <Layout>
+            <Image width="40%" src={images.gulp}/>
+            <Image width="40%" src={images.grunt}/>
+            <Image width="40%" src={images.broccoli}/>
+          </Layout>
+
+          <Heading>...</Heading>
+
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.but_what.replace("/", "")} bgDarken={0.75}>
+          <Heading size={2} fit caps lineHeight={1} textColor="primary">
+            Os problemas
+          </Heading>
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.plugins.replace("/", "")} bgDarken={0.75}>
+          <Heading size={2} fit caps lineHeight={1} textColor="primary">
+            Dependência de plugins
+          </Heading>
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.surprises.replace("/", "")} bgDarken={0.75}>
+          <Heading size={2} fit caps lineHeight={1} textColor="primary">
+            Comportamentos inesperados
+          </Heading>
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.surprises.replace("/", "")} bgDarken={0.75}>
+          <Heading size={2} fit caps lineHeight={1} textColor="primary">
+            Complicações desnecessárias
+          </Heading>
+        </Slide>
+
+        <Slide transition={["slide"]} bgImage={images.substack.replace("/", "")} bgDarken={0.75}>
+          <BlockQuote>
+            <Quote>There are some fancy tools for doing build automation on javascript projects that I have never felt the appeal of [...]</Quote>
+            <Cite>James "Substack" Halliday</Cite>
+          </BlockQuote>
         </Slide>
 
       </Deck>
